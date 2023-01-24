e47419ed0f3ab299e05d4b2de12343663ffe4c21
NikolayKhodakovskiy <122701385+NikolayKhodakovskiy@users.noreply.github.com>
2023-01-24 13:15:51 +0200

    Course work

---
+++ EncodingImages/CustomImage.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace EncodingImages
+{
+    internal class CustomImage
+    {
+        private Bitmap image;
+        private string projectPath;
+        private byte[] dataBytes;
+        private byte bitshift;
+
+        public CustomImage(Bitmap source, string data, string path, string imageName, string bitPosition)
+        {
+            image = source;
+            dataBytes = Encoding.Unicode.GetBytes(data);
+            projectPath = path + @"EncodedImages\" + imageName;
+            bitshift = Convert.ToByte(bitPosition);
+        }
+
+        public bool IsMessageLengthExceeded()
+        {
+            return ((image.Width - 1) * image.Height / 3) < dataBytes.Length;
+        }
+
+        public Bitmap EncodeTextIntoImage()
+        {
+            BitArray bits = new BitArray(dataBytes);
+            byte bitMask = (byte)(1 << bitshift);
+            int bit = bits.Length;
+            int counter = 0;
+            int x = 1;
+            SetPixelColor(0, 0, 1, ReturnBit(bitshift, 4), ReturnBit(bitshift, 2), ReturnBit(bitshift, 1));
+            for (; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (counter == 2)
+                    {
+                        SetPixelColor(x, y, bitMask, bits[--bit], bits[--bit], false);
+                        counter = 0;
+                    }
+                    else
+                    {
+                        SetPixelColor(x, y, bitMask, bits[--bit], bits[--bit], bits[--bit]);
+                        counter++;
+                    }
+                    if (bit == 0)
+                    {
+                        Color endMessagePixel = image.GetPixel(x, y);
+                        image.SetPixel(x, y, Color.FromArgb(endMessagePixel.R, endMessagePixel.G, EncodeBitPosition(endMessagePixel.B, true, bitMask)));
+                        goto EndLoop;
+                    }
+                }
+            }
+            EndLoop:
+            image.Save(projectPath);
+            return image;
+        }
+
+        void SetPixelColor(int x, int y, byte bitMask, params bool[] bits)
+        {
+            Color pixel = image.GetPixel(x, y);
+            byte red = EncodeBitPosition(pixel.R, bits[0], bitMask);
+            byte green = EncodeBitPosition(pixel.G, bits[1], bitMask);
+            byte blue = EncodeBitPosition(pixel.B, bits[2], bitMask);
+            image.SetPixel(x, y, Color.FromArgb(red, green, blue));
+        }
+
+        private byte EncodeBitPosition(byte color, bool bit, byte bitMask)
+        {
+            return (byte)( bit ? (color | bitMask) : (color & (~bitMask)) );
+        }
+
+        private static byte GetEncodedBitPosition(Bitmap image)
+        {
+            string bitShift = "";
+            Color pixel = image.GetPixel(0, 0);
+            bitShift += Convert.ToByte(ReturnBit(pixel.R, 1));
+            bitShift += Convert.ToByte(ReturnBit(pixel.G, 1));
+            bitShift += Convert.ToByte(ReturnBit(pixel.B, 1));
+            return Convert.ToByte(bitShift, 2);
+        }
+
+        public static string DecodeTextFromImage(string imagePath)
+        {
+            int bit = 0;
+            string decodedChar = "";
+            string initialMessage = "";
+            Bitmap image = new Bitmap(GetImageBitmap(imagePath));
+            byte bitMask = (byte)(1 << GetEncodedBitPosition(image));
+            for (int x = 1; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    decodedChar += Convert.ToByte(ReturnBit(pixel.R, bitMask));
+                    decodedChar += Convert.ToByte(ReturnBit(pixel.G, bitMask));
+                    byte flaggedBit = Convert.ToByte(ReturnBit(pixel.B, bitMask));
+                    if (bit == 2)
+                    {
+                        bit = 0;
+                        if (flaggedBit == 1) goto EndLoop;
+                    }
+                    else
+                    {
+                        decodedChar += flaggedBit;
+                        bit += 1;
+                    }
+                    if (decodedChar.Length == 16)
+                    {
+                        initialMessage += (char)Convert.ToInt16(decodedChar, 2);
+                        decodedChar = "";
+                    }
+                }
+            }
+            EndLoop:
+            if (decodedChar != string.Empty) initialMessage += (char)Convert.ToInt16(decodedChar, 2);
+
+            char[] temp = initialMessage.ToCharArray();
+            Array.Reverse(temp);
+            return new string(temp);
+        }
+
+        private static bool ReturnBit(byte number, byte bitMask)
+        {
+            return (number & bitMask) != 0;
+        }
+
+        public static bool IsExtensionSupported(string extension)
+        {
+            string[] extensions = { "bmp", "jpg", "jpeg", "png", "tiff" };
+            return extensions.Any(ext => ext == extension);
+        }
+
+        public static Bitmap GetImageBitmap(string path)
+        {
+            using (var temp = new Bitmap(path))
+            {
+                return new Bitmap(temp);
+            }
+        }
+
+        public static BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            MemoryStream stream = new MemoryStream();
+            bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            stream.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = stream;
+            image.EndInit();
+            return image;
+        }
+    }
+}
---
+++ EncodingImages/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace EncodingImages
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string projectPath;
+        private string imageTitle;
+        private string imagePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            projectPath = GetProjectPath();
+            imagePath = string.Empty;
+        }
+
+        private void LoadFromProject_Click(object sender, RoutedEventArgs e)
+        {
+            LoadImage(projectPath + "Images");
+        }
+
+        private void LoadFromResource_Click(object sender, RoutedEventArgs e)
+        {
+            LoadImage(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+        }
+
+        public string GetProjectPath()
+        {
+            string path = "";
+            string[] parts = Environment.CurrentDirectory.Split(@"\");
+            for (int i = 0; i < parts.Length - 3; i++)
+            {
+                path += parts[i] + @"\";
+            }
+            return path;
+        }
+
+        private void LoadImage(string path)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.InitialDirectory = path;
+            if (openFile.ShowDialog() == true)
+            {
+                imagePath = openFile.FileName;
+                imageTitle = imagePath.Split(@"\").Last();
+                if (!CustomImage.IsExtensionSupported(imageTitle.Split(".").Last()))
+                {
+                    MessageBox.Show("Формат выбранного файла не относится к изображениям\nили изображение такого формата не поддерживается", "Неверный формат", MessageBoxButton.OK);
+                    EncodedImage.Source = null;
+                    return;
+                }
+                Uri imageUri = new Uri(imagePath);
+                InitialImage.Source = new BitmapImage(imageUri);
+                EncodedImage.Source = null;
+            }
+        }
+        private void EncodeTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (InitialImage.Source == null)
+            {
+                MessageBox.Show("Сначала выберите картинку", "Отсутствие источника", MessageBoxButton.OK);
+                return;
+            }
+            if (Message.Text.Length == 0)
+            {
+                MessageBox.Show("Сначала введите текст", "Неверный ввод", MessageBoxButton.OK);
+                return;
+            }
+            Bitmap imageBitmap = CustomImage.GetImageBitmap(imagePath);
+            CustomImage image = new CustomImage(imageBitmap, Message.Text, projectPath, imageTitle, BitLabel.Text);
+            
+            MessageBoxResult result = image.IsMessageLengthExceeded() ? MessageBox.Show("Превышена длина текста.\nЕсли продолжить, информация будет обрезана", "Ошибка сообщения", MessageBoxButton.YesNo) : MessageBoxResult.Yes;
+            if (result == MessageBoxResult.Yes)
+            {
+                image.EncodeTextIntoImage();
+                EncodedImage.Source = CustomImage.ToBitmapImage(imageBitmap);
+                Message.Text = "";
+            }
+        }
+
+        private void DecodeTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EncodedImage.Source == null)
+            {
+                MessageBox.Show("Выбранное изображение, не содержало зашифрований текст", "Отсутствие источника", MessageBoxButton.OK);
+                return;
+            }
+            Message.Text = CustomImage.DecodeTextFromImage(projectPath + @"EncodedImages\" + imageTitle); 
+        }
+
+        private void ClearTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox box = (TextBox)sender;
+            box.Text = "";
+        }
+
+        private void ClearFields_Click(object sender, RoutedEventArgs e)
+        {
+            InitialImage.Source = null;
+            EncodedImage.Source = null;
+            BitPosition.Value = 0;
+            Message.Text = "";
+        }
+
+        private void ExitWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
